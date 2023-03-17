7911b2dad4ee00a567aecad771c3035dc09001d4
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-17 11:21:36 -0500

    created string assignment

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // defines strings
+            string sentence1 = "Goose and ";
+            string sentence2 = "Pigeon and ";
+            string sentence3 = "Mirabelle are cats!";
+
+            // concatenates strings
+            string concat = sentence1 + sentence2 + sentence3;
+            // converts string to uppercase
+            concat = concat.ToUpper();
+
+            Console.WriteLine(concat);
+
+            // creates a StringBuilder and initializes with first sentence
+            StringBuilder sb = new StringBuilder("Hello, my name is Shannon. ");
+
+            // adds to end of StringBuilder
+            sb.Append("Today it is 11 degrees and sunny ");
+            sb.Append("in rural Wisconsin. ");
+            sb.Append("Hopefully tomorrow will be warmer.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
