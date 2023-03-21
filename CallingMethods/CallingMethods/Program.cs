5e66eaaf7ecde92e8d94ffa66579f736663ad47a
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-21 18:14:44 -0500

    Created Calling Methods assignment

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompts user to enter a number to perform math operation, converts to int data type
+            Console.WriteLine("Enter a number to perform math operations on:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            UserInput userInput = new UserInput();
+
+            // calls each method and passes in user input
+            int divideTwo = userInput.DivideTwo(userNum);
+            int addFifty = userInput.AddFifty(userNum);
+            int multiplyFive = userInput.MultiplyFive(userNum);
+
+            // displays returned integer
+            Console.WriteLine(userNum + " divided by 2 equals " + divideTwo);
+            Console.WriteLine(userNum + " plus 50 equals " + addFifty);
+            Console.WriteLine(userNum + " multiplied by 5 equals " + multiplyFive);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/userInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class UserInput
+    {
+        // creates three methods that takes in one int parameter and returns one integer
+        public int DivideTwo(int p)
+        {
+            return p / 2;
+        }
+
+        public int AddFifty(int p)
+        {
+            return p + 50;
+        }
+
+        public int MultiplyFive(int p)
+        {
+            return p * 5;
+        }
+    }
+}
