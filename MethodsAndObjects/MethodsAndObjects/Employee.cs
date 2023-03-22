9b3e092cd846725376c0627faf2eea86014a72de
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-22 12:28:49 -0500

    Created Methods and Objects assignment

---
+++ MethodsAndObjects/MethodsAndObjects/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    // creates class that inherits from Person class
+    class Employee : Person
+    {
+       // creates class property with int data type
+        public int Id { get; set; }
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Person
+    {
+       // creates class properties with string data type
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // creates a void method with no parameters, displays full name to console
+        public void SayName()
+        {
+            string fullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: " + fullName);
+            Console.ReadLine();
+        }
+    }
+}
