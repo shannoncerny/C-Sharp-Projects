57b409e6759fef758f883ced76195abac2f9a905
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-21 20:11:09 -0500

    Created Main Method assignment

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates the class and calls the method, passing in an integer
+            MathMethod mathMethod = new MathMethod();
+            int mathOperation = mathMethod.MathOperation(22);
+            Console.WriteLine("22 plus 50 equals " + mathOperation);
+
+            // instantiates the class and calls the second method, passing in a decimal
+            MathMethod mathMethod2 = new MathMethod();
+            int mathOperation2 = mathMethod2.MathOperation(22.5M);
+            Console.WriteLine("22.5 divided by 3 equals " + mathOperation2);
+
+            // instantiates the class and calls the third method, passing in a string
+            MathMethod mathMethod3 = new MathMethod();
+            int mathOperation3 = mathMethod3.MathOperation("11");
+            Console.WriteLine("11 multiplied by 5 equals " + mathOperation3);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethod/MainMethod/UserInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class MathMethod
+    {
+        // creates method that takes in an integer and adds 50
+        public int MathOperation(int p)
+        {
+            return (p + 50);
+        }
+
+        // creates method that takes in a decimal and divides by 3
+        public int MathOperation(decimal p)
+        {
+            return (int)(p / 3M);
+        }
+
+        // creates method that takes in a string, converts to int, multiplies by 5
+        public int MathOperation(string p)
+        {
+            if (int.TryParse(p, out int value))
+            {
+                return value * 5;
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid input.");
+            }
+        }
+    }
+}
