bcdb9ad931f60e4d9594f416dee63b0ada4935a0
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-22 14:00:37 -0500

    Created Polymorphism assignment

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    // creates class that inherits interface
+    public class Employee : IQuittable
+    {
+        public int Id { get; set; }
+
+        public void Quit()
+        {
+            Console.WriteLine("Quitting program...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creates object of type IQuittable, calls Quit method
+            IQuittable employee = new Employee();
+            employee.Quit();
+        }
+    }
+}
