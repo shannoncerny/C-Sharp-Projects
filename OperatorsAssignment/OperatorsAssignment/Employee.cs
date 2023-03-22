6502e7b31bd0d05e116916f9802c5e1fbcfdcff1
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-22 14:40:10 -0500

    Created Operators assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        // creates properties with int data type and string data types
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // overload "==" operator, checks if 2 employee objects are equal by checking Id
+        public override bool Equals(object obj)
+        {
+            Employee employee = (Employee)obj;
+            if (Id == employee.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(Employee employee, Employee employee2)
+        {
+            return employee.Equals(employee2);
+        }
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            return !employee.Equals(employee2);
+        }
+
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates class objects, assigns values to the properties
+            Employee employee = new Employee() 
+            { 
+                Id = 1,
+                FirstName = "Jane", 
+                LastName = "Doe" 
+            };
+            Employee employee2 = new Employee()
+            {
+                Id = 2,
+                FirstName = "Bob",
+                LastName = "Smith"
+            };
+            // compares the employee objects using overloaded operators, displays results
+            if (employee == employee2)
+            {
+                Console.WriteLine("Employee is equal to Employee2.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Employee is not equal to Employee2.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
