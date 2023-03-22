59f7c3f1d960c6b814ae8ba70bfd81106b464144
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-22 11:14:01 -0500

    Created Method Class assignment

---
+++ MethodClass/MethodClass/MathMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class MathMethod
+    {
+        // creates void method that takes two integers as parameters
+        public void MathOperation(int p, int p2)
+        {
+            // performs math opreation on first int, displays second int 
+            int sum = p + 50;
+            Console.WriteLine(sum);
+            Console.WriteLine(p2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates the class
+            MathMethod mathMethod = new MathMethod();
+            // calls class method, passes in two numbers
+            mathMethod.MathOperation(25, 80);
+            // calls class method, specifies numbers by parameter name
+            mathMethod.MathOperation(p: 25, p2: 80);
+        }
+    }
+}
