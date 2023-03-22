89266887fe1ef5d497c308896f186acab884b959
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-22 11:03:42 -0500

    Created Method submission assignment

---
+++ Method/Method/MathMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class MathMethod
+    {
+        // creates method tthat takes two integers as parameters, one optional
+        // performs math operation and returns int result
+        public int MathOperation(int p, int p2 = 2)
+        {
+            return p * p2;
+        }
+    }
+}
---
+++ Method/Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates the class
+            MathMethod mathMethod = new MathMethod();
+            //prompts user to input two integers, one of them optional
+            Console.WriteLine("Enter a number:");
+            string userNum = Console.ReadLine();
+            Console.WriteLine("Enter a second number (optional):");
+            string userNum2 = Console.ReadLine();
+
+            // calls the class method and passes in the user input
+            if (userNum2 != "")
+            {
+                int mathOperation = mathMethod.MathOperation(Convert.ToInt32(userNum), Convert.ToInt32(userNum2));
+                Console.WriteLine(userNum + " * " + userNum2 + " = " + mathOperation);
+            }
+            else
+            {
+                int mathOperation = mathMethod.MathOperation(Convert.ToInt32(userNum));
+                Console.WriteLine(userNum + " * 2 = " + mathOperation);
+            }
+            Console.ReadLine();
+        }
+    }
+}
