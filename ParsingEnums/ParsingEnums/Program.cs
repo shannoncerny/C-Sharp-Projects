5014132a696020cd49bebd3a532f690f12e75b7f
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-23 15:40:38 -0500

    Created Parsing Enums assignment

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompts user to enter current day of week
+            Console.WriteLine("Please enter the day of the week:");
+            string userInput = Console.ReadLine();
+
+            // try and catch block to display error message if error occurs
+            try
+            {
+                // assigns value to a variable of enum data type
+                DaysOfTheWeek daysOfTheWeek = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userInput);
+                Console.WriteLine("You entered: " + daysOfTheWeek.ToString());
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
