7f8a9112c0053378efa1970248a43107870a1b7e
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-24 11:56:08 -0500

    Created DateTime assignment

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // prints cuurrent date & time to console
+        Console.WriteLine("Current date: " + DateTime.Now);
+
+        // prompts user to enter number
+        Console.WriteLine("Enter a number:");
+        string userNum = Console.ReadLine();
+        double userNumDouble = Convert.ToDouble(userNum);
+        
+        // prints exact time it will be in x hours, x = user input
+        DateTime newTime = DateTime.Now.AddHours(userNumDouble);
+        Console.WriteLine("In {0} hours, it will be {1}.", userNum, newTime);
+        Console.ReadLine();
+    }
+}
+
