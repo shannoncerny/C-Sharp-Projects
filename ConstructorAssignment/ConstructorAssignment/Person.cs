b9cca9f8cf61b34a20fc9deba79c68516cb2a410
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-27 15:10:03 -0500

    Created Constructor assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    // chains two constructors together
+   public class Person
+    {
+        public string Middle;
+        public string First;
+        public string Last;
+
+        public Person(string lastName, string firstName) : this(lastName, firstName, "N/A")
+        {
+            Console.WriteLine("Constructed");
+        }
+
+        public Person(string lastName, string firstName, string middleName)
+        {
+            Middle = middleName;
+            First = firstName;
+            Last = lastName;
+            Console.WriteLine(firstName + " " + middleName + " " + lastName);
+        }
+        
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creates a const variable
+            const string name = "Shannon";
+            Console.WriteLine("Hello, {0}!", name);
+
+            // creates variable using "var"
+            var x = 47;
+            Console.WriteLine("The variable is {0}", x);
+
+            var person1 = new Person("Cerny", "Shannon");
+            var person2 = new Person("Cerny", "Shannon", "Nicole");
+
+            Console.ReadLine();
+        }
+    }
+}
