f622ca17f2e37f5a0fa15b29f6c396b62fe25d10
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-03-27 15:49:19 -0500

    Created Try/Catch Assignment

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1- Ask the user for their age.
+            try
+            {
+                Console.WriteLine("Please enter your age");
+                int usersAge = Convert.ToInt32(Console.ReadLine());
+                //Step 4- Display appropriate error messages if the user enters zero
+                //or negative numbers.
+                if (usersAge <= 0)
+                {
+                    Console.WriteLine("Please enter a number greater than zero.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    //Determines year of birth
+                    int usersBirthYear = 2023 - usersAge;
+
+                    //Step 2-Display the year the user was born
+                    //Step 3- Handle exceptions using "try/catch".
+
+                    Console.WriteLine("You were born in the year {0}.", usersBirthYear);
+                    Console.ReadLine();
+                }
+
+            }
+            catch (Exception)
+            {
+                //Step 5- Display a general message if an exception was caused by anything else.
+                Console.WriteLine("An error occured, please contact your system admin.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
