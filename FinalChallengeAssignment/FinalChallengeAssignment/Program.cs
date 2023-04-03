90b632b76493b15e4a6811f19468c64ef6e903ba
Shannon Cerny <118932978+shannoncerny@users.noreply.github.com>
2023-04-02 19:31:37 -0500

    Created Final Challenge assignment

---
+++ FinalChallengeAssignment/FinalChallengeAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using FinalChallengeAssignment;
+using System;
+using System.Linq;
+
+namespace FinalChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                Console.WriteLine("Enter your first name:");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter your last name:");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Enter your age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                Student student = new Student
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age
+                };
+
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                var query = from b in db.Students
+                            orderby b.LastName
+                            select b;
+                Console.WriteLine("All students in the database: \n");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Id + ": " + item.LastName + ", " + item.FirstName);
+                }
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ FinalChallengeAssignment/FinalChallengeAssignment/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalChallengeAssignment
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext()
+            : base("name=Students")
+        {
+        }
+        public virtual DbSet<Student> Students { get; set; }
+    }
+    public class Student
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
